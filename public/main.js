02f11df1330a75bf336ba09fca3dc1624f09cf7c
Phlongs <phlongs@gmail.com>
2017-10-13 20:35:20 -0400

    update

--- public/main.js
+++
@@ -1,7 +0,0 @@
-  // $(function () {
-  //   $("button").on("click", function (event) {
-  //     e.preventDefault();
-  //     $(".hidden-control").attr("type", "text");
-  //     $("form").submit();
-  //   });
-  // });
