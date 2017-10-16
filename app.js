34524c485e820961f9d13affb9000096ef836b78
Phlongs <phlongs@gmail.com>
2017-10-15 20:56:40 -0400

    heroku

--- app.js
+++ app.js
@@ -60,17 +60,19 @@ var upload = multer({ storage: storage });
 
 // Routes
 app.get("/", (req, res) => {
-	GameInfo.find().populate('gamePublisher').populate('gamePublisher.publisher').exec(function(err, data){
-		if(err) {
-			console.log(err)
-		} else {
-			
-			res.render('gameList', {
-				datas: data
-			})		
-		}
 
-	});
+	res.send('test');
+	// GameInfo.find().populate('gamePublisher').populate('gamePublisher.publisher').exec(function(err, data){
+	// 	if(err) {
+	// 		console.log(err)
+	// 	} else {
+			
+	// 		res.render('gameList', {
+	// 			datas: data
+	// 		})		
+	// 	}
+
+	// });
 });
 
 
